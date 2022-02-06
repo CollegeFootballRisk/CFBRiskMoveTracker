ec04db37477dd7181e194d318967d92461e92c91
Brendan F <EpicWolverine@users.noreply.github.com>
2022-02-05 20:41:03 -0500

    Add risk cache testing

--- risk_api.py
+++ risk_api.py
@@ -59,7 +59,13 @@ class RiskApi:
 
 
 class MockRiskApi(RiskApi):
+    def __init__(self):
+        super().__init__()
+        self._get_team_api_data_access_count = {"players": 0, "mercs": 0}
+        self._get_player_api_data_access_count = 0
+
     def _get_team_api_data(self, endpoint):
+        self._get_team_api_data_access_count[endpoint] += 1
         if endpoint == "players":
             return json.loads('[{"team": "Aldi", "player": "user1", "turnsPlayed": 16, "mvps": 6, "lastTurn": {"season": 1, "day": 17, "stars": 3}}, {"team": "Aldi", "player": "EpicWolverine", "turnsPlayed": 112, "mvps": 9, "lastTurn": {"season": 1, "day": 17, "stars": 4}}, {"team": "Aldi","player": "user2","turnsPlayed": 92,"mvps": 12,"lastTurn": {"season": -1,"day": 116,"stars": 4}}]')
         elif endpoint == "mercs":
@@ -68,6 +74,7 @@ class MockRiskApi(RiskApi):
             return super()._get_team_api_data(endpoint)
 
     def _get_player_api_data(self, player_name):
+        self._get_player_api_data_access_count += 1
         if player_name == "EpicWolverine":
             return json.loads('{"active_team": {"name": "Aldi"},"name": "EpicWolverine","platform": "reddit","ratings": {"awards": 5,"gameTurns": 3,"mvps": 4,"overall": 4,"streak": 4,"totalTurns": 5},"stats": {"awards": 5,"gameTurns": 18,"mvps": 10,"streak": 18,"totalTurns": 113},"team": {"name": "Aldi"},"turns": [{"day": 18,"mvp": true,"season": 1,"stars": 4,"team": "Aldi","territory": "Alaska"},{"day": 17,"mvp": false,"season": 1,"stars": 4,"team": "Aldi","territory": "Minnesota"}]}')
         elif player_name == "user1":
@@ -102,3 +109,17 @@ class TestSuite(unittest.TestCase):
                       {'day': 17, 'mvp': False, 'season': 1, 'stars': 4, 'team': 'Aldi', 'territory': 'Minnesota'}]}
         self.assertEqual(self.cut.get_player_info("EpicWolverine"), expected)
 
+    def test_get_players_cache(self):
+        self.cut.get_players()
+        self.cut.get_players()
+        self.assertEqual(self.cut._get_team_api_data_access_count["players"], 1)
+
+    def test_get_mercs_cache(self):
+        self.cut.get_mercs()
+        self.cut.get_mercs()
+        self.assertEqual(self.cut._get_team_api_data_access_count["mercs"], 1)
+
+    def test_player_info_cache(self):
+        self.cut.get_player_info("EpicWolverine")
+        self.cut.get_player_info("EpicWolverine")
+        self.assertEqual(self.cut._get_player_api_data_access_count, 1)
