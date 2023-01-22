9705f4415c08806a579c5e320c5321abff7e54cb
Brendan F <EpicWolverine@users.noreply.github.com>
2023-01-21 22:30:23 -0800

    Support retry on all Discord requests

--- discord_api.py
+++ discord_api.py
@@ -17,34 +17,8 @@ class DiscordApi:
     def launch_bot_auth(self):
         webbrowser.open(f"{self.api_endpoint}/oauth2/authorize?client_id={self.secrets['client_id']}&scope=bot&permissions=134217728&guild_id={self.secrets['guild_id']}&disable_guild_select=true")
 
-    def get_bot_id(self):
-        if self.bot_id is None:
-            r = requests.get(f"{self.api_endpoint}/users/@me", headers=self.headers)
-            r.raise_for_status()
-            self.bot_id = r.json()['id']
-        return self.bot_id
-
-    def get_guild_member(self, discord_id):
-        r = requests.get(f"{self.api_endpoint}/guilds/{self.secrets['guild_id']}/members/{discord_id}", headers=self.headers)
-        r.raise_for_status()
-        return r.json()
-
-    def get_guild_members(self):
-        params = {"limit": 1000}
-        r = requests.get(f"{self.api_endpoint}/guilds/{self.secrets['guild_id']}/members", params=params, headers=self.headers)
-        r.raise_for_status()
-        return r.json()
-
-    def get_guild_member_ids(self):
-        ids = []
-        for guild_member in self.get_guild_members():
-            ids.append(guild_member['user']['id'])
-        return ids
-
-    def set_nickname(self, discord_id, nickname):
-        Logger.log(f"Setting {discord_id} to \"{nickname}\"")
-        body = {"nick": nickname}
-        r = requests.patch(f"{self.api_endpoint}/guilds/{self.secrets['guild_id']}/members/{discord_id}", json=body, headers=self.headers)
+    def call_api_get(self, url, params=None) -> dict:
+        r = requests.get(url, params=params, headers=self.headers)
         response = r.json()
         try:
             r.raise_for_status()
@@ -55,8 +29,47 @@ class DiscordApi:
                 delay = response["retry_after"]
                 Logger.log(f"Waiting {delay} seconds.")
                 time.sleep(delay)
-                response = self.set_nickname(discord_id, nickname)
+                response = self.call_api_get(url, params)
         return response
+
+    def get_bot_id(self):
+        if self.bot_id is None:
+            self.bot_id = self.call_api_get(f"{self.api_endpoint}/users/@me")['id']
+        return self.bot_id
+
+    def get_guild_member(self, discord_id):
+        return self.call_api_get(f"{self.api_endpoint}/guilds/{self.secrets['guild_id']}/members/{discord_id}")
+
+    def get_guild_members(self):
+        params = {"limit": 1000}
+        return self.call_api_get(f"{self.api_endpoint}/guilds/{self.secrets['guild_id']}/members", params=params)
+
+    def get_guild_member_ids(self):
+        ids = []
+        for guild_member in self.get_guild_members():
+            ids.append(guild_member['user']['id'])
+        return ids
+
+    def call_api_patch(self, url, body) -> dict:
+        r = requests.patch(url, json=body, headers=self.headers)
+        response = r.json()
+        try:
+            r.raise_for_status()
+        except Exception as e:
+            Logger.log(response)
+            Logger.log(e)
+            if "retry_after" in response:
+                delay = response["retry_after"]
+                Logger.log(f"Waiting {delay} seconds.")
+                time.sleep(delay)
+                response = self.call_api_patch(url, body)
+        return response
+
+    def set_nickname(self, discord_id, nickname):
+        Logger.log(f"Setting {discord_id} to \"{nickname}\"")
+        url = f"{self.api_endpoint}/guilds/{self.secrets['guild_id']}/members/{discord_id}"
+        body = {"nick": nickname}
+        return self.call_api_patch(url, body)
 
     def use_test_guild(self):
         self.secrets["guild_id"] = self.secrets["test_guild_id"]
