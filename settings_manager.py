6cd7fcb1836974437b6e9aea04c7476d5e81542c
Brendan F <EpicWolverine@users.noreply.github.com>
2024-04-20 01:00:43 -0700

    Add checks for missing settings

--- settings_manager.py
+++ settings_manager.py
@@ -1,16 +1,24 @@
 import json
 
+SETTINGS_FILE_NAME = "settings.json"
+
 
 class SettingsManager:
     def __init__(self):
-        with open("settings.json") as f:
+        with open(SETTINGS_FILE_NAME) as f:
             self.settings = json.load(f)
 
     def get_secrets(self):
-        return self.settings["secrets"]
+        secrets = self.settings.get("secrets")
+        if secrets is None:
+            raise KeyError(f'"secrets" and its child keys must be defined in {SETTINGS_FILE_NAME}')
+        return secrets
 
     def get_team_name(self):
-        return self.settings["settings"]["team"]
+        team = self.settings.get("settings").get("team")
+        if team is None:
+            raise KeyError(f'"settings.team" must be defined in {SETTINGS_FILE_NAME}')
+        return team
 
     def get_verified_discord_role_name(self):
-        return self.settings["settings"]["verified_discord_role_name"]
+        return self.settings.get("settings").get("verified_discord_role_name")
